f31a28a81b1075e8f15560b8ecb4d0340a61e02e
Tommy McLeroy <tmcleroy@gmail.com>
2015-02-12 00:40:00 -0600

    added jquery, did basic setup for pixi

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,12 +5,42 @@
 requirejs.config({
   baseUrl: '../bower_components/',
   paths: {
-    lodash: 'lodash/lodash'
+    lodash: 'lodash/lodash',
+    jquery: 'jquery/dist/jquery',
+    pixi: 'pixi/bin/pixi'
   }
 });
 
 requirejs([
-  'lodash'
-], function (_) {
-  console.log('LOGLOGLOG');
+  'lodash',
+  'jquery',
+  'pixi'
+], function (_, $, Pixi) {
+  var stage = new Pixi.Stage(0xCCCCCC);
+  var renderer = Pixi.autoDetectRenderer(640, 360);
+  document.body.appendChild(renderer.view);
+
+  // set up animation loop
+  requestAnimFrame(animate);
+
+  var texture = Pixi.Texture.fromImage('assets/images/bunny.png');
+  var bunny = new Pixi.Sprite(texture);
+
+  // center the sprites anchor point
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // center the sprite
+  bunny.position.x = renderer.width / 2;
+  bunny.position.y = renderer.height / 2;
+
+  stage.addChild(bunny);
+
+  function animate() {
+      requestAnimFrame(animate);
+
+      bunny.rotation += 0.1;
+
+      renderer.render(stage);
+  }
 });
